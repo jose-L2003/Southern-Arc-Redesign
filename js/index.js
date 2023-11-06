de00bb3f8016c63c309a6663b53549adb2ef5462
Jose <joselimon110903@gmail.com>
2023-11-05 21:46:01 -0600

    Added content to homepage

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-var myName = "Jose"
+function myFunction () {
+    var moreText = document.getElementById("more")
+    var btnText = document.getElementById;("myBtn");
 
-console.log(myName)
+    if (moreText.style.display === "none") {
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "Read more";
+    } else {
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
